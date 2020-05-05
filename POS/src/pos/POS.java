ba14c2df5a5b00353920c971231a4b7f7ff65a6d
Alexander Shapiro <me@alexanderjshapiro.com>
2020-05-04 17:53:35 -0700

    added grocery dashboard, assets, and runner

--- POS/src/pos/POS.java
+++ POS/src/pos/POS.java
@@ -1,8 +1,12 @@
 package pos;
 
+import javax.swing.JFrame;
+
 /**
+ * Runner for Dashboard.java
  *
- * @author alex
+ * @author Alexander Shapiro, Edwin Mejia
+ * @version 0.1
  */
 public class POS {
 
@@ -10,7 +14,11 @@ public class POS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Grocery grocery = new Grocery();
+        
+        grocery.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        grocery.setVisible(true);
+
     }
     
 }
